913f3aa340bec4728d27777cd4cdf8d4e5587eff
Prashant-SL <pacchidon07@gmail.com>
2022-04-16 20:50:49 +0530

    Hello

--- city-country/src/components/Home/Home.jsx
+++ city-country/src/components/Home/Home.jsx
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 const Home = () => {
-	return <>Add Home</>;
+	// const data=[];
+	const [cityData, setCityData] = useState([]);
+	useEffect(() => {
+		getData();
+		// console.log(cityData);
+	}, []);
+	var details;
+	const getData = async () => {
+		const response = await axios.get('http://localhost:8080/cities');
+		var details = response.data;
+		setCityData(...cityData, response.data);
+		console.log('cityData:', cityData);
+	};
+	return (
+		<>
+			<table>
+				<thead>
+					<tr>
+						<th>id</th>
+						<th>Country</th>
+						<th>City</th>
+						<th>Population</th>
+						<th>Edit</th>
+						<th>Delete</th>
+					</tr>
+				</thead>
+				<tbody>
+					{cityData.map((e) => {
+						<tr key={e.id}>
+							<td>{e.id}</td>
+							<td>{e.country}</td>
+							<td>{e.city}</td>
+							<td>{e.population}</td>
+						</tr>;
+					})}
+				</tbody>
+			</table>
+		</>
+	);
 };
 export default Home;
